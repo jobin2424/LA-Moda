f91a9b4106987c2fccb6a61d7daf37b094cd604f
jobin2424 <102490710+jobin2424@users.noreply.github.com>
2022-05-09 15:27:28 -0400

    Add files via upload

---
+++ LA Moda/app.js
@@ -0,0 +1,16 @@
+
+const ham = document.getElementById('ham');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (ham) {
+    ham.addEventListener('click', () => {
+        nav.classList.add('active')
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
